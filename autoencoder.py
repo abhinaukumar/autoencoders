a59d6cc57c4b3f31e019d9ab838f0474e8370232
Abhinau <ab.kumr98@gmail.com>
2017-03-22 19:26:42 +0530

    Initial commit.

---
+++ autoencoder.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 19:21:12 2017
+
+@author: nownow
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 11:09:40 2017
+
+@author: nownow
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  5 13:01:36 2017
+
+@author: nownow
+"""
+
+from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.models import Model,Sequential
+import numpy as np
+from random import shuffle
+from keras.callbacks import TensorBoard
+
+from keras.datasets import mnist
+
+import os
+#import tensorflow as tf
+import numpy as np
+from PIL import Image
+import cv2
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 1, 28, 28))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 1, 28, 28))  # adapt this if using `channels_first` image data format
+
+print("Read dataset");
+
+#done reading images
+
+#model
+
+input_img = Input(shape=(1, 64, 64))
+
+x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(input_img)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
+encoded = MaxPooling2D((2, 2), border_mode='same')(x)
+
+# at this point the representation is (8, 4, 4) i.e. 128-dimensional
+
+x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Convolution2D(1, 3, 3, activation='relu', border_mode='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+
+print("Model created and compiled")
+
+autoencoder.fit(x_train,x_train,nb_epoch = 50, batch_size = 128, shuffle = False, validation_data=(x_test, x_test),verbose = 2, callbacks=[TensorBoard(log_dir='/tmp/mnist_autoencoder')])
