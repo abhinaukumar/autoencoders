6892ea0ddef57bf18cec366e1e08cebefafc2c6f
Abhinau <ab.kumr98@gmail.com>
2017-07-02 02:21:07 +0530

    Clean up.

--- main_file.py
+++ main_file.py
@@ -10,7 +10,9 @@ import keras
 import manifold_script
 a = str(input("Enter number: "))
 
-generator = keras.models.load_model('/home/nownow/Documents/projects/idp_summer_2017/conv_decoder_must_be_best_model.json')
+path_to_file = ""
+
+generator = keras.models.load_model(path_to_file)
 manifold_script.show_digits(generator,a)
 
 choice = raw_input("Do you wish to see the manifold? (y/n): ")
