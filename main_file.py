63090c4e7f0d7f5746105eddf1a77c16c4cf64ab
Abhinau <ab.kumr98@gmail.com>
2017-07-14 18:14:22 +0530

    Fixed bug

--- main_file.py
+++ main_file.py
@@ -10,12 +10,13 @@ import keras
 import manifold_script
 a = str(raw_input("Enter number: "))
 
-path_to_file = ''
+path_to_file = '/home/nownow/Documents/projects/idp_summer_2017/saved_model/conv_decoder_2_best.json'
 generator = keras.models.load_model(path_to_file)
 manifold_script.show_digits(generator,a)
 
 choice = raw_input("Do you wish to see the manifold? (y/n): ")
 if choice == 'y':
-    manifold_script.show_manifold(generator)
+    for i in range(10):
+        manifold_script.show_manifold(generator,i)
     
 print("Exiting..")
